47845f6e29bf2a927fb494b49f23d51297858e52
Ahmed Sherif <ahmedsherif91101@gmail.com>
2024-05-06 03:35:49 +0300

    before login and register forms 7 5 2024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,46 @@
-import Image from "next/image";
+import { FC } from "react";
 
-export default function Home() {
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+const Home: FC = () => {
   return (
-    <div> hello </div>
+    <main
+      className="h-full flex flex-col items-center justify-center
+      bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]
+      from-sky-400 to-blue-800"
+    >
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg text-center">
+          Asimple authentication service
+        </p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign In
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
---
+++ src/components/auth/social.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { FcGoogle } from "react-icons/fc";
+
+import { Button } from "@/components/ui/button";
+
+export const Social = () => {
+  return (
+    <div className="w-full flex items-center gap-x-2">
+      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+        <FcGoogle className="h-5 w-5" />
+      </Button>
+    </div>
+  );
+};
